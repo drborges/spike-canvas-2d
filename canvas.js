a182d1b9a4b2fb98e80b890fa27e5f73f5ef8e4d
Diego Borges <drborges.cic@gmail.com>
2015-02-15 00:15:11 -0200

    playing around with canvas 2d

---
+++ canvas.js
@@ -0,0 +1,44 @@
+window.onload = function() {
+  var myCanvas = document.getElementById("myCanvas");
+  var curColor = $('#selectColor option:selected').val();
+  if(myCanvas){
+    var isDown      = false;
+    var ctx = myCanvas.getContext("2d");
+    var canvasX, canvasY;
+    ctx.lineWidth = 1;
+
+    $(myCanvas)
+    .mousedown(function(e){
+      isDown = true;
+      ctx.beginPath();
+      canvasX = e.pageX - myCanvas.offsetLeft;
+      canvasY = e.pageY - myCanvas.offsetTop;
+      ctx.moveTo(canvasX, canvasY);
+    })
+    .mousemove(function(e){
+      if(isDown) {
+        canvasX = e.pageX - myCanvas.offsetLeft;
+        canvasY = e.pageY - myCanvas.offsetTop;
+        ctx.lineTo(canvasX, canvasY);
+        ctx.strokeStyle = curColor;
+        ctx.stroke();
+      }
+    })
+    .mouseenter(function (e) {
+      ctx.beginPath();
+      ctx.moveTo(canvasX, canvasY);
+    })
+    .mouseout(function (e) {
+      ctx.closePath();
+    })
+    .mouseup(function(e){
+      isDown = false;
+      ctx.closePath();
+    });
+  }
+
+  $('#selectColor').change(function () {
+    curColor = $('#selectColor option:selected').val();
+  });
+
+};
